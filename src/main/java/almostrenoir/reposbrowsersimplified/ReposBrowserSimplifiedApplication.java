7f8e40d2020775d2a15f31fbd0f728b212076e17
Rafał Augustyn <r.augustyn96@gmail.com>
2023-09-05 05:45:45 +0200

    init spring app

---
+++ src/main/java/almostrenoir/reposbrowsersimplified/ReposBrowserSimplifiedApplication.java
@@ -0,0 +1,13 @@
+package almostrenoir.reposbrowsersimplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposBrowserSimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposBrowserSimplifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/almostrenoir/reposbrowsersimplified/ReposBrowserSimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package almostrenoir.reposbrowsersimplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposBrowserSimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
